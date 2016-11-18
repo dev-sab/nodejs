57c36b6554f671cdc585ccba9822b2602b9580d1
sabarish3012 <sabarish.appiness@gmail.com>
2016-11-18 13:09:48 +0530

    a

--- nodejs/node_learn/rouxmeet/app.js
+++ nodejs/node_learn/rouxmeet/app.js
@@ -23,6 +23,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+// Import data.jsom from here
+
+var data=require('./data.json');
+app.locals.appdata=data;
+
 //app.use('/users', users);
 
 // catch 404 and forward to error handler
--- nodejs/node_learn/rouxmeet/routes/index.js
+++ nodejs/node_learn/rouxmeet/routes/index.js
@@ -1,9 +1,66 @@
 var express = require('express');
 var router = express.Router();
+var appdata=require('../data.json');
+
+//console.log(appdata);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	// rende index.ejs
+  //res.render('index', { title: 'Express' });
+  var myartwork=[];
+  var myartists=[];
+  myartists=appdata.speakers;
+  appdata.speakers.forEach(function(item){
+  	myartwork=myartwork.concat(item.artwork);
+  });
+  console.log('myartwork');
+  console.log(myartwork);
+  res.render('index', { 
+  	title: 'Home' ,
+  	artwork:myartwork,
+    artists:myartists,
+    page:'home'
+  });
+});
+
+
+/* GET Speakers page. */
+router.get('/speakers', function(req, res, next) {
+  var myartwork=[];
+  var myartists=[];
+  myartists=appdata.speakers;
+  appdata.speakers.forEach(function(item){
+    myartwork=myartwork.concat(item.artwork);
+  });
+  res.render('speakers', { 
+    title: 'Speakers' ,
+    artwork:myartwork,
+    artists:myartists,
+    page:'artistList'
+  });
+});
+
+
+/* GET Speakers page. */
+router.get('/speakers/:speakerId', function(req, res, next) {
+  var myartwork=[];
+  var myartists=[];
+
+  //speakerId is a local variable
+  appdata.speakers.forEach(function(item){
+    if(item.shortname==req.params.speakerId){
+      myartists.push(item);
+      myartwork=myartwork.concat(item.artwork);  
+    }
+  });
+
+  res.render('speakers', { 
+    title: 'Speakers' ,
+    artwork:myartwork,
+    artists:myartists,
+    page:'artistDetail'
+  });
 });
 
 module.exports = router;
